86074166eb4ac3d901bc50bda85e1e64e8201080
KMyrcik <krzysztof.myrcik@300brains.com>
2023-05-19 11:31:05 +0200

    script complete

--- turtle_race.py
+++ turtle_race.py
@@ -1,6 +1,9 @@
 import turtle, time
+import random
 
 WIDTH, HEIGHT = 500, 500
+COLORS = ["red", "green", "blue", "cyan", "orange", "yellow", "black", "purple", "pink", "brown"]
+
 print("Welcome to turtle racing! Please enter the following informations: ")
 
 def get_number_of_racers():
@@ -10,13 +13,41 @@ def get_number_of_racers():
         if turtle_number.isdigit():
             turtle_number = int(turtle_number)
         else:
-            print("Imput is not numeric...Try Again")
+            print("Input is not numeric...Try Again")
             continue
 
         if 2 <= turtle_number <= 10:
             return turtle_number
         else:
             print("number not in range 2-10. Try again")
+
+def race(colors):
+    turtles = create_turtles(colors)
+
+    while True:
+        for racer in turtles:
+            distance = random.randrange(1, 20)
+            racer.forward(distance)
+
+            x, y = racer.pos()
+            if y >= HEIGHT // 2 - 10:
+                return colors[turtles.index(racer)]
+
+
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color)
+        racer.shape('turtle')
+        racer.left(90)
+        racer.penup()
+        racer.setpos(-WIDTH//2 + (i + 1) * spacingx, -HEIGHT//2 + 15)
+        racer.pendown()
+        turtles.append(racer)
+
+    return turtles
 
 
 def init_turtle():
@@ -27,30 +58,9 @@ def init_turtle():
 
 racers = get_number_of_racers()
 init_turtle()
+random.shuffle(COLORS)
+colors = COLORS[:racers]
 
-racer = turtle.Turtle()
-racer.speed(1)
-racer.penup()
-racer.shape("turtle")
-racer.color("cyan")
-racer.forward(100)
-racer.left(90)
-racer.pendown()
-racer.forward(100)
-racer.left(80)
-racer.backward(40)
-
-racer2 = turtle.Turtle()
-racer2.speed(1)
-racer2.penup()
-racer2.shape("circle")
-racer2.color("red")
-racer2.forward(100)
-racer2.left(90)
-racer2.pendown()
-racer2.forward(100)
-racer2.left(80)
-racer2.backward(40)
-
-
+winner = race(colors)
+print(winner + " turtle is a new champion")
 time.sleep(5)
