6f0dd90d3c48f722f08edd15e4f5f316b77b3f77
KMyrcik <krzysztof.myrcik@300brains.com>
2023-05-22 10:02:55 +0200

    tic tc toe start of creation

---
+++ kolko_krzyzyk.py
@@ -0,0 +1,60 @@
+import turtle, time, random
+
+
+WIDTH, HEIGHT = 500, 500
+x_left_corner = -300
+y_left_corner = 300
+distance = int(WIDTH // 3)
+
+
+def init_window():
+    screen = turtle.Screen()
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("Tic Tac Toe game!")
+    screen.bgcolor("black")
+    return screen
+
+
+# shape siatka
+def init_mesh(x, y, distance):
+    hero = init_object()
+    for a in [1, 2]:
+        hero.penup()
+        hero.goto(x + a * distance, y)
+        hero.pendown()
+        hero.goto(x + a * distance, y)
+
+        hero.penup()
+        hero.goto(x, y - a * distance)
+        hero.pendown()
+        hero.goto(- x, y - a * distance)
+
+
+def init_object():
+    hero = turtle.Turtle()
+    hero.color("white")
+    hero.pensize(7)
+    hero.speed(0)
+    hero.hideturtle()
+    return hero
+
+
+window = init_window()
+init_mesh(x_left_corner, y_left_corner, distance)
+time.sleep(5)
+
+table = [[None, None, None],
+        [None, None, None],
+        [None, None, None]]
+
+#adresowanie [wiersz],[kolumna]
+
+turn = random.choice(['x','o'])
+
+window.onclick(click)
+
+def click(x,y):
+        pass
+
+window.listen()
+window.mainloop()
